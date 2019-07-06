81db02e53002f2c2e579bd6b423fcc5ff8059111
clickingmouse <clickingmouse@users.noreply.github.com>
2019-07-06 16:47:22 +0800

    debugged basic appbar & added menu placeholder

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,5 +1,46 @@
 import React from "react";
-import AppBar from "material-ui/AppBar";
-const NavBar = () => <AppBar title="Majestic7Guesthouse" />;
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const NavBar = () => {
+  const classes = useStyles();
+  return (
+    <AppBar color="primary" position="static">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="Menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" className={classes.title}>
+          Majestic7Guesthouse
+        </Typography>
+        <Button color="inherit">Menu1</Button>
+        <Button color="inherit">Menu2</Button>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default NavBar;
