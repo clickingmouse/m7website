984ba95b7168f7d573dd629382596929b53ccae7
clickingmouse <clickingmouse@users.noreply.github.com>
2019-07-09 10:11:40 +0800

    added basic grids for landing & footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+
 import NavBar from "./components/navbar/NavBar";
+import Landing from "./components/layout/landing/Landing";
+import Footer from "./components/layout/footer/Footer";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <div>
-          <NavBar />;
-        </div>
-      </MuiThemeProvider>
+      <div>
+        <NavBar />;
+        <Landing />
+        <Footer />
+      </div>
     );
   }
 }
---
+++ src/components/layout/footer/Footer.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+const useStyles = makeStyles(theme => ({
+  root: {
+    //width: "100%",
+    marginTop: theme.spacing(3),
+    overflowX: "auto"
+  },
+  table: {
+    //minWidth: 650
+  }
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9)
+];
+
+const Footer = () => {
+  const classes = useStyles();
+
+  function FormRow() {
+    return (
+      <React.Fragment>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+        <Grid item xs={4}>
+          <Paper className={classes.paper}>item</Paper>
+        </Grid>
+      </React.Fragment>
+    );
+  }
+
+  return (
+    <div className={classes.root}>
+      <hr />
+      <br />
+      <Grid container spacing={1}>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item xs={12} spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+      All Rights Reserved
+    </div>
+  );
+};
+
+export default Footer;
