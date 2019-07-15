92d9ded061f23c4f06c07e51cec00a0c4c0ffd23
clickingmouse <clickingmouse@users.noreply.github.com>
2019-07-15 15:24:40 +0800

    basic landing content

--- src/components/layout/landing/Landing.js
+++ src/components/layout/landing/Landing.js
@@ -3,6 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import Slider from "./Slider";
+
+import victoriasferry from "../../../images/landing/victoriaharbourferry.jpg";
+import victoriasPeakView from "../../../images/landing/VictoriasPeak.jpg";
+import PeakTramView from "../../../images/landing/PeakTramView01.jpg";
+const aboutText =
+  "Conveniently located in the heart of Tsim Sha Tsui, Majestic 7 Guesthouse (formerly KAT Hotel) offers accommodation in one of Hong Kong's most vibrant neighborhoods. We are within a two-minute walk from the MTR Tsim Sha Tsui station, and within ten minutes walk from the Star Ferry and Kowloon Park, as well as near many shopping districts, night markets, food vendors, and museums. /n\n\t\b We are a family-run guesthouse and have native English, Cantonese and Tagalog speakers. Our affordable air-conditioned rooms are quietly situated on the seventh floor of an apartment building and each room has its own private, attached bathroom. It is the perfect base for an adventurous traveller!";
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
@@ -11,22 +18,40 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%"
   }
 }));
 
-const Landing = () => {
+const Landing = (about = aboutText) => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
         <Grid item xs={4}>
           <Paper className={classes.paper}>xs</Paper>
+          <img className={classes.img} src={victoriasferry} alt="hongkong" />
         </Grid>
         <Grid item xs={4}>
-          <Paper className={classes.paper}>xs=4</Paper>
+          <Paper className={classes.paper}>
+            xs=4
+            <br />
+            <strong>About</strong>
+            <br />
+            {aboutText}
+          </Paper>
         </Grid>
         <Grid item xs={4}>
           <Paper className={classes.paper}>xs</Paper>
+          <img
+            className={classes.img}
+            src={PeakTramView}
+            alt="Hong Kong Peak View"
+          />
         </Grid>
       </Grid>
       <Grid container spacing={3}>
