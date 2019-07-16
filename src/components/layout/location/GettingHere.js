a26728d769b99b5d5069c7fd770ee6ef04fae882
clickingmouse <clickingmouse@users.noreply.github.com>
2019-07-16 10:48:32 +0800

    change img max-width to width 100%

--- src/components/layout/location/GettingHere.js
+++ src/components/layout/location/GettingHere.js
@@ -6,6 +6,9 @@ import Grid from "@material-ui/core/Grid";
 import Map from "./Map";
 import FromHKIA from "./FromHKIA";
 
+import entrance from "../../../images/location/majestic-house-entrance.jpg";
+import outrance from "../../../images/location/majestic-house-entrance-outview.jpg";
+
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
@@ -14,6 +17,15 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(2),
     textAlign: "center",
     color: theme.palette.text.secondary
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    width: "100%",
+    //maxWidth: "100%",
+    height: "auto",
+    //maxHeight: "auto",
+    objectFit: "contain"
   }
 }));
 
@@ -25,7 +37,15 @@ const GettingHere = () => {
       GETTING HERE SECTION
       <Grid container spacing={3}>
         <Grid item xs>
-          <Paper className={classes.paper}>xs</Paper>
+          <Paper className={classes.paper}>
+            xs
+            <br />
+          </Paper>
+          <img
+            className={classes.img}
+            src={entrance}
+            alt="Majestic House Building Entrance"
+          />
         </Grid>
         <Grid item xs>
           <Paper className={classes.paper}>xs</Paper>
