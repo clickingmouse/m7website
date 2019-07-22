0ece1b02ecbcf5492c71dbf96c2bb48baebd0298
clickingmouse <clickingmouse@users.noreply.github.com>
2019-07-22 18:37:56 +0800

    fixed hero parallax

--- src/components/layout/landing/Hero.js
+++ src/components/layout/landing/Hero.js
@@ -2,9 +2,21 @@ import React, { Component } from "react";
 import tstArielImg from "../../../images/landing/Tsim_Sha_Tsui_Ariel.jpg";
 const Hero = () => {
   return (
-    <div className="parallax-container">
-      <div className="parallax">
-        <img src={tstArielImg} />
+    <div>
+      <div className="parallax-container">
+        <div className="parallax">
+          <img src={tstArielImg} />
+        </div>
+      </div>
+      <div className="section white">
+        <div className="row container">
+          <h2 className="header">Parallax</h2>
+          <p className="grey-text text-darken-3 lighten-3">
+            Parallax is an effect where the background content or image in this
+            case, is moved at a different speed than the foreground content
+            while scrolling.
+          </p>
+        </div>
       </div>
     </div>
   );
