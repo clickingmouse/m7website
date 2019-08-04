7c024606e7f06738ce3dfb57a0a83b3afbb95544
clickingmouse <clickingmouse@users.noreply.github.com>
2019-08-04 22:20:36 +0800

    started building list for rooms cards

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -2,44 +2,69 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 //import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
+import AppBar from "@material-ui/core/AppBar";
+//import {Fragment} from 'react'
+import { Toolbar } from "@material-ui/core";
 
 import Address from "./Address";
 import ContactInfo from "./ContactInfo";
 import cyan from "@material-ui/core/colors/cyan";
-const footerPrimary = cyan["A200"]; //#18ffff
+import indigo from "@material-ui/core/colors/indigo";
+import { createMuiTheme } from "@material-ui/core/styles";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+//const footerPrimary = cyan["A200"]; //#18ffff
+const footerPrimary = indigo["500"]; //#3f51b5
 
 const useStyles = makeStyles(theme => ({
   root: {
     //width: "100%",
     marginTop: theme.spacing(3),
     marginBottom: theme.spacing(2),
-    overflowX: "auto",
-    backgroundColor: footerPrimary
+    overflowX: "auto"
+    //backgroundColor: footerPrimary
   },
   table: {
     //minWidth: 650
+  },
+  toolBar: {
+    backgroundColor: footerPrimary
   }
 }));
-
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: {
+      main: "#f44336"
+    }
+  }
+});
 const Footer = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      BOTTOM FOOTER
-      <Grid container spacing={1}>
-        <Grid container item xs={3} spacing={3} />
-        <Grid container item xs={3} spacing={3}>
-          <Address />
-        </Grid>
-        <Grid container item xs={3} spacing={3}>
-          <ContactInfo />
-        </Grid>
+    <React.Fragment>
+      <div className={classes.root}>
+        <MuiThemeProvider theme={theme}>
+          <Toolbar className={classes.toolBar}>tool bar</Toolbar>
+        </MuiThemeProvider>
+      </div>
 
-        <Grid container item xs={3} spacing={3} />
-      </Grid>
-      All Rights Reserved
-    </div>
+      <div className={classes.root} color="inherit">
+        BOTTOM FOOTER
+        <Grid container spacing={1}>
+          <Grid container item xs={3} spacing={3} />
+          <Grid container item xs={3} spacing={3}>
+            <Address />
+          </Grid>
+          <Grid container item xs={3} spacing={3}>
+            <ContactInfo />
+          </Grid>
+
+          <Grid container item xs={3} spacing={3} />
+        </Grid>
+        All Rights Reserved
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/components/layout/rooms/BudgetCard.js
+++ src/components/layout/rooms/BudgetCard.js
@@ -7,12 +7,23 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
 
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Divider from "@material-ui/core/Divider";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Icon from "@material-ui/core/Icon";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import Label from "@material-ui/icons/Label";
 import budgetImg from "../../../images/rooms/budget.jpg";
 const useStyles = makeStyles({
   card: {
     maxWidth: "100%"
-  }
+  },
+  ul: { listStyleType: "square!important" },
+  li: { listStyleType: "square!important" }
 });
 
 const BudgetCard = () => {
@@ -32,9 +43,29 @@ const BudgetCard = () => {
           <Typography gutterBottom variant="h5" component="h2">
             Budget
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+          <Paper>
+            <List>
+              <ListItem>
+                <ListItemIcon>
+                  <Label />
+                </ListItemIcon>
+                <ListItemText primary="Best Deal in Town" />
+              </ListItem>
+              <Divider />
+              <ListItem>
+                <ListItemIcon>
+                  <Label />
+                </ListItemIcon>
+                <ListItemText primary="Windowless" />
+              </ListItem>
+            </List>
+          </Paper>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="subtitle1"
+          >
+            ...
           </Typography>
         </CardContent>
       </CardActionArea>
