ecbccc8e9d9ae01c4ef328dbe5653598ad9086b0
clickingmouse <clickingmouse@users.noreply.github.com>
2019-08-05 12:24:19 +0800

    removed room cards place holder text

--- src/components/layout/rooms/SingleCard.js
+++ src/components/layout/rooms/SingleCard.js
@@ -58,8 +58,7 @@ const SingleCard = () => {
             </List>
           </Paper>
           <Typography variant="body2" color="textSecondary" component="p">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            ...
           </Typography>
         </CardContent>
       </CardActionArea>
