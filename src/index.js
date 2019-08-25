a80df16fba8be3e4c9f6f5b3aeb294b2d1f42ae3
clickingmouse <clickingmouse@users.noreply.github.com>
2019-08-25 14:00:45 +0800

    removed materializecss parallax and reonnected auth.isReady

--- src/index.js
+++ src/index.js
@@ -40,26 +40,26 @@ const store = createStore(
 
 store.firebaseAuthIsReady.then(() => {
   // this breaks parallax
+
+  //3.0
+  //const rrfProps = {
+  //  firebase,
+  //  config: rrfConfig,
+  //  dispatch: store.dispatch
+  //createFirestoreInstance // <- needed if using firestore 3.0
+  //};
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </Provider>,
+    document.getElementById("root")
+  );
 }); //const authisReady
-//3.0
-//const rrfProps = {
-//  firebase,
-//  config: rrfConfig,
-//  dispatch: store.dispatch
-//createFirestoreInstance // <- needed if using firestore 3.0
-//};
-
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <MuiThemeProvider>
-        <App />
-      </MuiThemeProvider>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById("root")
-);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
